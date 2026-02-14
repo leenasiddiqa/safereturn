ffc528793483b0a9d6d09477fa66fd06b63f61a8
Usva-Qandeel <46416@students.riphah.edu.pk>
2026-02-14 20:13:19 +0500

    complete project

--- src/backend/models/contact.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const contactSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  message: { type: String, required: true },
-  contactDate: { type: Date, default: Date.now }
-});
-
-export default mongoose.model("Contact", contactSchema);
