d260d3d21f8528c55b6425c683223b1a3ed92a6c
Usva-Qandeel <46416@students.riphah.edu.pk>
2026-04-30 17:34:49 +0500

    change login page

--- src/backend/models/login.js
+++ src/backend/models/login.js
@@ -1,13 +1,10 @@
 import mongoose from "mongoose";
 
-// ✅ Login Schema (Simple version - sirf authentication ke liye)
 const loginSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Signup', required: true } // Signup se link karega
 }, {
-  timestamps: true // automatically createdAt aur updatedAt add karega
+  timestamps: true 
 });
-
-// ✅ Model export karo
 export default mongoose.model("Login", loginSchema);
